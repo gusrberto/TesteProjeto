e137117f2348740e07a69c2c9590fa131ee1cd3d
gusrberto <gustavorsdemelo@gmail.com>
2024-09-10 23:08:09 -0300

    log dbPool result

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -7,4 +7,18 @@ const dbPool = mysql.createPool({
     database: process.env.DB_NAME,
 });
 
+(async () => {
+    try {
+        // Testa a conexão ao banco de dados
+        const connection = await dbPool.getConnection();
+        console.log("Conexão com o banco de dados estabelecida com sucesso!");
+        connection.release(); // Libera a conexão de volta ao pool
+    } catch (error: unknown) {
+        if (error instanceof Error)
+            console.error("Erro ao conectar ao banco de dados:", error.message);
+        else
+            console.error("Erro desconhecido");
+    }
+})();
+
 export default dbPool;
